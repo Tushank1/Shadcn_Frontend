a792d865ede97d700641570b019df6b6d0b187a1
Tushank Bhardwaj <tushankbhardwaj121@gmail.com>
2025-02-22 17:06:33 +0530

    fourth commit

--- src/components/Hero/pages/Hero1.jsx
+++ src/components/Hero/pages/Hero1.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "../pages_style/Hero1.css";
 import Header from "../../Header/Header";
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { GiHangingSpider } from "react-icons/gi";
+import { RiComputerLine } from "react-icons/ri";
+import { IoMdTabletPortrait } from "react-icons/io";
+import { AiOutlineMobile } from "react-icons/ai";
+import { MdOutlineFitScreen } from "react-icons/md";
 
 const Hero1 = () => {
   return (
@@ -28,7 +34,41 @@ const Hero1 = () => {
               </div>
             </div>
             <div className="hero1_main_inner_container_top_right">
-              <span>right</span>
+              <div className="hero1_main_inner_container_top_right_center">
+                <div className="hero1_main_inner_container_top_right_center_left">
+                  <div className="hero1_main_inner_container_top_right_center_left_content">
+                    <div className="hero1_main_inner_container_top_right_center_left_content_left">
+                      <GiHangingSpider />
+                    </div>
+                    <div className="hero1_main_inner_container_top_right_center_left_content_right">
+                      <div className="hero1_main_inner_container_top_right_center_left_content_right_left">
+                        <div className="hero1_main_inner_container_top_right_center_left_content_right_left_first">
+                          <RiComputerLine />
+                        </div>
+                        <div className="hero1_main_inner_container_top_right_center_left_content_right_left_second">
+                          <IoMdTabletPortrait />
+                        </div>
+                        <div className="hero1_main_inner_container_top_right_center_left_content_right_left_third">
+                          <AiOutlineMobile />
+                        </div>
+                      </div>
+                      <div className="hero1_main_inner_container_top_right_center_left_content_right_right">
+                        <MdOutlineFitScreen />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="hero1_main_inner_container_top_right_center_right">
+                  <div className="hero1_main_inner_container_top_right_center_right_content">
+                    <div className="hero1_main_inner_container_top_right_center_right_content_left">
+                      <MdOutlineShoppingBag />
+                    </div>
+                    <div className="hero1_main_inner_container_top_right_center_right_content_right">
+                      <span>Copy Code</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="hero1_main_inner_container_bottom">
